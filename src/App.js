5706d6204f53a79fc97c33563be19cab76be89e5
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-11-25 15:24:05 +1100

    initiated the chat app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./modules/Form";
+
+function App() {
+  return (
+    <div className="bg-[#edf3fc] h-screen">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, jsx, ts, tsx}"],
+  theme: {
+    colors: {
+      primary: "#1476ff",
+      secondary: "#f3f5ff",
+      light: "#f9faff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
